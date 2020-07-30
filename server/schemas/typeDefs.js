10513141e852b70d175469fc01a026699bed8bc5
Aaron Lisker <aelisker@gmail.com>
2020-07-29 17:40:35 -0700

    initial commit

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,25 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type Book {
+
+  }
+
+  type User {
+
+  }
+
+  type Auth {
+
+  }
+
+  type Query {
+
+  }
+
+  type Mutation {
+
+  }
+`;
+
+module.exports = typeDefs;
